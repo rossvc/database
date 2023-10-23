f31ef7658bf0557452f0e7e0a55caaa722e456d2
jike <jessicaike123@gmail.com>
2023-10-22 22:01:52 -0500

    Made changes to Front and admmissions page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
 //Not Found page
-
-const cards = [1, 2, 3];
 
 export default function NotFound() {
   return (
