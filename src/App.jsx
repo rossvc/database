e5eec5f88fc636c306368829a0beb1ee53d6f08a
Alejandro Perez Gonzalez <ale07pg@gmail.com>
2023-11-04 10:17:43 -0500

    first admn page commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
-import { useState, useEffect } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import FrontPage from './Pages/FrontPage';
-import Admission from './Pages/Admission';
-import GiftShop from './Pages/GiftShop';
-import Exhibition from './Pages/Exhibition';
-import Artworks from './Pages/Artworks';
-import Login from './Components/Login';
-import Footer from './Components/Footer';
-import DefaultAppBar from './Components/DefaultAppBar';
-import NotFound from './Pages/NotFound';
-import EmployeeLanding from './Pages/EmployeeLanding';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import FrontPage from "./Pages/FrontPage";
+import Admission from "./Pages/Admission";
+import GiftShop from "./Pages/GiftShop";
+import Exhibition from "./Pages/Exhibition";
+import Artworks from "./Pages/Artworks";
+import Login from "./Components/Login";
+import Footer from "./Components/Footer";
+import Administrator from "./Pages/Administrator";
+import DefaultAppBar from "./Components/DefaultAppBar";
+import NotFound from "./Pages/NotFound";
+import EmployeeLanding from "./Pages/EmployeeLanding";
 
 //This is app, this shows the website, in order for the website to be viewed you must include it in this file
 
@@ -19,18 +20,18 @@ function App() {
   const theme = createTheme({
     palette: {
       primary: {
-        main: '#FF5733', // Set your desired orange color
+        main: "#FF5733", // Set your desired orange color
       },
     },
     overrides: {
       MuiButton: {
         contained: {
-          color: '#FF5733', // Text color
-          backgroundColor: '#FFFFFF', // Button background color
-          '&:hover': {
-            backgroundColor: '#FF5733', // Button background color on hover
-            '@media (hover: none)': {
-              backgroundColor: '#FFFFFF', // Reset on touch devices
+          color: "#FF5733", // Text color
+          backgroundColor: "#FFFFFF", // Button background color
+          "&:hover": {
+            backgroundColor: "#FF5733", // Button background color on hover
+            "@media (hover: none)": {
+              backgroundColor: "#FFFFFF", // Reset on touch devices
             },
           },
         },
@@ -51,32 +52,32 @@ function App() {
   const handleLogin = async (username, password) => {
     // Add login in logic
   };
-  
 
   //Here we store user info, handle log in and log out logic
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <DefaultAppBar
-          onLogin={openLoginModal}
-        />
+        <DefaultAppBar onLogin={openLoginModal} />
         <Login
           open={isLoginModalOpen}
           onClose={closeLoginModal}
           onLogin={handleLogin}
         />
-          <Router>
-            <Routes> {/*TODO: Add user auth in element sections*/}
-              <Route path="/" element={<FrontPage />} /> 
-              <Route path="/admission" element={<Admission />} />
-              <Route path="/giftshop" element={<GiftShop />} />
-              <Route path="/exhibition" element={<Exhibition />} />
-              <Route path="/artworks" element={<Artworks />} />
-              <Route path="*" element={<NotFound />} />
-              <Route path="/employeelanding" element={<EmployeeLanding />} />
-            </Routes>
-          </Router>
-        <Footer/>
+        <Router>
+          <Routes>
+            {" "}
+            {/*TODO: Add user auth in element sections*/}
+            <Route path="/" element={<FrontPage />} />
+            <Route path="/admission" element={<Admission />} />
+            <Route path="/giftshop" element={<GiftShop />} />
+            <Route path="/exhibition" element={<Exhibition />} />
+            <Route path="/artworks" element={<Artworks />} />
+            <Route path="/administrator" element={<Administrator />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/employeelanding" element={<EmployeeLanding />} />
+          </Routes>
+        </Router>
+        <Footer />
       </ThemeProvider>
     </div>
   );
