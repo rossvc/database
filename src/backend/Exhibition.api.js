28f1c240e4436bb91483a50929661fd23670915d
ksskatka <kasparslothskatka@gmail.com>
2023-11-06 02:39:47 -0600

    Create exhibition api file, add api definitions

--- src/backend/Exhibition.api.js
+++ src/backend/Exhibition.api.js
@@ -1,34 +1,34 @@
 const baseURL = "https://ross.fail:3001/";
 
-export const addArtCollection = async (itemBody) => {
+export const addExhibition = async (itemBody) => {
     try {
         const requestOptions = {
             method: 'POST',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify(itemBody)
         }
-        const response = await fetch(baseURL+"api/collections", requestOptions);
+        const response = await fetch(baseURL+"api/exhibitions", requestOptions);
         if (!response.ok) {
             throw new Error( `Fetch error: ${response.status}` )
         }
         const data = await response.json();
         return data.data;
     } catch (error) {
-        console.error('Error adding art collection:', error);
+        console.error('Error adding exhibition:', error);
         throw error;
     }
 };
 
-export const getAllArtCollections = async () => {
+export const getExhbitions = async () => {
     try {
-        const response = await fetch(baseURL+"api/collections");
+        const response = await fetch(baseURL+"api/exhibitions");
         if (!response.ok) {
             throw new Error(`Fetch error: ${response.status}`);
         }
         const data = await response.json();
         return data.data;
     } catch (error) {
-        console.error('Error getting all art collections', error);
+        console.error('Error getting all exhibitions:', error);
         throw error;
     }
 };
