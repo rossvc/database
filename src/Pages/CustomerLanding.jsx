eba6f5facfd9d53304b3a461a96c2a3f6885d26f
jike <jessicaike123@gmail.com>
2023-11-15 13:45:21 -0600

    Fixed but

--- src/Pages/CustomerLanding.jsx
+++ src/Pages/CustomerLanding.jsx
@@ -13,8 +13,6 @@ import { getCustomerRecepits } from '../backend/Customer.api';
 // Here customers can use their email address to get a list of all of their purchases, including ticket sales and giftshop sales
 // They will be grouped by order
 
-
-
 const ticketColumns = [
   { field: 'SaleType', headerName: 'Purchase Made', width: 200, },
   { field: 'PurchaseDate', headerName: 'Sale Date', width: 250, },
